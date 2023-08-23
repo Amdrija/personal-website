1e885a889f419cb0ebe5acbc6ada67737727c95b
Andrija Jelenkovic <andrija.jelenkovic@protonmail.com>
2023-08-23 18:13:00 +0200

    Fix github link not visible on iphone, add autoclose nav on click for phones

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,12 +1,16 @@
 window.addEventListener('DOMContentLoaded', function() {
   const burger = document.getElementById("burger");
   const nav = this.document.getElementById("nav");
+  const navLinks = nav.querySelectorAll("ul li a");
 
-  burger.addEventListener('click', () => {
-    if (nav.classList.contains("active")) {
-      nav.classList.remove("active");
-    } else {
-      nav.classList.add("active");
-    }
-  })
-})
+  burger.addEventListener('click', toggleMenu);
+  navLinks.forEach(link => link.addEventListener('click', toggleMenu));
+});
+
+function toggleMenu() {
+  if (nav.classList.contains("active")) {
+    nav.classList.remove("active");
+  } else {
+    nav.classList.add("active");
+  }
+}
